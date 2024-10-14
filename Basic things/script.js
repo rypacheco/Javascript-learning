0de1355a249a8e0f72d442e26ea08403cdeb4b28
Ryan Araujo <ryanaraujoxd@gmail.com>
2024-10-14 14:14:10 -0300

    introdução a javascript

---
+++ Basic things/script.js
@@ -0,0 +1,9 @@
+// sintaxe correta.
+console.log("Olá, Haru!")
+
+// sintaxe incorreta.
+// con.sole.log("Olá, Haru!")
+
+// No Javascript ponto e vericula é opcional
+console.log("Olá, Haruzada");
+console.log("Olá, Haruziinho!")
