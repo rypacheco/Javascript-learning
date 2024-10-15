b9e4c19349f506e760b45d3e864cfdca11f148af
Ryan Araujo <ryanaraujoxd@gmail.com>
2024-10-15 11:59:13 -0300

    exercicio de escopo

---
+++ Variaveis e constantes/Escopos/script.js
@@ -0,0 +1,30 @@
+// console.log(user)
+// var user = "Haru"
+
+// Hoisting
+// var user
+// console.log(user)
+
+// Escopo Global
+var email = "haru@gmail.com"
+
+{ 
+   // Escopo de bloco.
+    console.log(email)
+}
+
+
+var age 
+{
+
+var age = 18
+    
+}
+
+console.log(age)
+
+let adress = "rua X"
+{
+    let adress = "rua W"
+    console.log(adress)
+}
