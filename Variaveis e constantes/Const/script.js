f5bdf77e3a4d1fa1d299e7feed5307965978aed2
Ryan Araujo <ryanaraujoxd@gmail.com>
2024-10-15 11:08:05 -0300

    novas atualizações incluindo escopos, let, const e hoisting.

---
+++ Variaveis e constantes/Const/script.js
@@ -0,0 +1,9 @@
+// Uma constante cria uma variável cujo valor é fixo (não pode ser alterado)
+
+const number  = 42
+console.log(number)
+
+// irá gerar um erro porque o valor não pode ser alterado.
+
+number = 55
+console.log(number)
---
+++ Variaveis e constantes/Let/script.js
@@ -0,0 +1,12 @@
+// Declara uma variável sem valor.
+
+let user
+console.log("Haru")
+
+// Declara uma variável com valor.
+
+let email ="harugoth@email.com"
+console.log(email)
+
+email = "harugothkaisa@gmail.com"
+console.log(email)
