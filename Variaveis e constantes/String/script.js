42d24f33bbfe99ac0643b7051f10406f3ee04e5a
Ryan Araujo <ryanaraujoxd@gmail.com>
2024-10-17 14:28:51 -0300

    adicionando as aulas de tring e tipos de dados.

---
+++ Variaveis e constantes/String/script.js
@@ -0,0 +1,19 @@
+// String
+
+let = usernaname = "Haru"
+console.log(usernaname)
+console.log(typeof usernaname)
+
+console.log ("Uma string com aspas")
+console.log ('uma string com apóstrofo (aspas simples)')
+
+// Quando utilziar uma ou outra?
+console.log('Uma tring com "aspas duplas" dentro de simples')
+
+console.log ("uma string com 'aspas simples' dentro de duplas")
+
+
+console.log(`
+    uma string com acento grave permite 
+    escrever multiplas linhas.
+    `)
