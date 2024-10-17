93c9ab92da41b88049471bea97706f73b279256b
Ryan Araujo <ryanaraujoxd@gmail.com>
2024-10-17 10:34:53 -0300

    aula sobre nomes para variaveis, usando case-sensitive com camelCase e snake_case

---
+++ Variaveis e constantes/nomes-para-variaveis/script.js
@@ -0,0 +1,35 @@
+// case-sensitive 
+
+let username = "Haru"
+let userName = "Yoshida"
+
+console.log (username)
+console.log(userName)
+
+
+// PODEMOS
+let $email = "haru@gmail.com"
+let _email = "yoshida@gmail.com"
+let Ação = "Cadastrar"
+let user_email = "manu@gmail.com"
+
+console.log($email)
+console.log(_email)
+console.log(user_email)
+
+console.log(Ação)
+
+// NÃO PODEMOS
+
+// let 1user = "Manu" => variável que inicia com numero
+
+// RECOMENDAÇÕES - camelCase (concurnda do camelo)
+
+let productName = "Headset"
+let firstName = "Haru"
+let lastName = "Pacheco"
+
+// RECOMENDAÇÃO snake_casa = (rastejar_underline)
+let product_name = "Headset"
+let first_name = "Haru"
+let last_name = "Pacheco"
