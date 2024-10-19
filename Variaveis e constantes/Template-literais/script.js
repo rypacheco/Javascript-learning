28fe3779eecda18ab3e2f28bf2b31fc0ed06d95d
Ryan Araujo <ryanaraujoxd@gmail.com>
2024-10-18 23:12:20 -0300

    aula de template literals

---
+++ Variaveis e constantes/Template-literais/script.js
@@ -0,0 +1,15 @@
+// Template literals (template strings, interpolação de strings)
+let username = "Haru"
+let email = "haru@gmail.com"
+
+// passar mais de um parâmetro
+console.log(username, email, "teste")
+
+// Concatenar texto
+
+let message = "Olá," + username + ". Você conectou com o e-mail" + email
+
+console.log(message)
+
+
+console.log (`Olá, ${username}. Você conectou com o e-mail ${email}. `)
