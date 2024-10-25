6f9fa893999f58cf85118454caf5506920215005
haru <ryanaraujoxd@gmail.com>
2024-10-25 12:39:47 -0300

    aula sobre operadores.

---
+++ Expressões e Operadores/Operadores-aritmeticos/script.js
@@ -0,0 +1,16 @@
+// Operadores aritméticos 
+
+console.log("SOMA: ", 12 + 8)
+console.log("CONCATENAÇÃO: ", "12" + 8) // 128
+
+console.log("SUBTRAÇÃO: ", 12 - 8)
+
+
+console.log("MULTIPLICAÇÃO: ", 3 * 5.5)
+
+
+console.log("DIVISÃO: ", 12 / 2)
+
+console.log("RESTO DA DIVISÃO: " , 13 % 2)
+
+console.log("EXPONENCIAL:" , 3 ** 3)
---
+++ Expressões e Operadores/Operadores/script.js
@@ -0,0 +1,5 @@
+// Expressão aritmética que soma dois números
+let sum = 4 + 3 + 7;
+
+// expressão aritmética que calcula a média
+let average = sum / 3
