26f7d715f6ceab0343ea428ba24c5bced0d9de49
haru <ryanaraujoxd@gmail.com>
2024-10-29 09:34:09 -0300

    operadores de atribuição

---
+++ Expressões e Operadores/operadores de atribuição/script.js
@@ -0,0 +1,30 @@
+let value
+
+// operador de atribuição 
+value = 1
+console.log(value)
+
+// incremento
+value += 2 
+console.log(value)
+
+// decremento
+value -= 2
+console.log(value)
+
+// multiplicar 
+value *= 3
+console.log (value)
+
+// divisão 
+value /=2
+console.log(value)
+
+// resto da divisão
+
+value %= 2
+console.log(value)
+
+// potência
+value **= 2
+console.log(value)
