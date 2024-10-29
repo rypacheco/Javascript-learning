545eeaff2da2f36a6d2f93d5e970df9fa01fdeb9
haru <ryanaraujoxd@gmail.com>
2024-10-29 08:28:32 -0300

    aula de incremento e decremento de operadores

---
+++ Expressões e Operadores/incremento-e-decremento/script.js
@@ -0,0 +1,30 @@
+let number = 10
+
+
+// incremento 
+// number = number + 1
+number++
+console.log(number++)
+console.log(number)
+
+// incrementa antes
+console.log(++number)
+
+// Decremento
+console.log("decremento após: ", number--)
+console.log(number--)
+console.log("Decremnento antes: ", --number)
+
+
+// incrementar mais de um. (colocar)
+number += 10 
+console.log(number)
+
+// derementar mais de um. (remover)
+number -= 2
+console.log(number)
+
+number = number + 20
+console.log(number)
+number = number - 20
+console.log(number)
