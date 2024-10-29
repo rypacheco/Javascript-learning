679f77e798f0fdd6a861578255a0d6378cf2672e
haru <ryanaraujoxd@gmail.com>
2024-10-29 09:25:21 -0300

    aulas de estritamente igual e diferente

---
+++ Expressões e Operadores/Estritamente Igual e Diferente/script.js
@@ -0,0 +1,13 @@
+let one = 1 
+let two = 2
+
+
+// === estritamente igual a (tipo e valor)
+console.log(one === 1)
+console.log(one === "1")
+
+// !== estritamente diferente de 
+console.log(one !== two)
+console.log(one !== 1)
+console.log(two !== 2)
+console.log(two !== "2")
---
+++ Expressões e Operadores/Igual a e Diferente de/script.js
@@ -0,0 +1,13 @@
+let one = 1
+let two = 2
+
+// == igual a
+console.log(one == two)
+console.log(one == 1)
+console.log(one == "1")
+
+// != Diferente de
+console.log("### DIFERENTE DE ###")
+console.log (one != two)
+console.log(one != 1)
+console.log(one != "1")
