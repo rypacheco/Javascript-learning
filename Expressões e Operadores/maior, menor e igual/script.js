7430e59f297863756bad605eb681edf24b932b34
haru <ryanaraujoxd@gmail.com>
2024-10-29 09:29:56 -0300

    aula de operadores maior, menor e igual

---
+++ Expressões e Operadores/maior, menor e igual/script.js
@@ -0,0 +1,16 @@
+let balance = 500
+let payment = 120
+
+// > Maior que
+console.log(balance > payment)
+
+// < Menor que 
+console.log(balance < payment)
+
+// >= Maior ou igual a 
+balance = 120
+console.log(balance >= payment)
+
+// <= menor ou igual a
+balance = 500
+console.log(balance<=payment)
