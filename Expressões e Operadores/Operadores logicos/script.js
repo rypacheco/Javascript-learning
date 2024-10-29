dad5a90b738ad8dcbbc105c37f5e6efaf750494a
haru <ryanaraujoxd@gmail.com>
2024-10-29 09:41:09 -0300

    aula de operadores logicos

---
+++ Expressões e Operadores/Operadores logicos/script.js
@@ -0,0 +1,13 @@
+let email = true
+// let password = false (teste para verificar dando falso ao usar And &&)
+let password = false
+
+
+// And (E) && (vai retornar verdadeiro se todas as condições forem verdadeiras)
+console.log(email && password)
+
+// Or (ou) || 
+console.log(email || password)
+
+// NOT (negação) ! 
+console.log(!password)
