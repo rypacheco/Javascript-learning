18e29eee2ecf821199acce5ac02dbab4df6e9af6
haru <ryanaraujoxd@gmail.com>
2024-10-29 09:03:42 -0300

    exercicio utilizando grouping operator

---
+++ Expressões e Operadores/ordem-de-precedencia/script.js
@@ -0,0 +1,10 @@
+// Grouping Operator (ordem de precedência)
+
+let total1 = 2+3 * 4
+console.log(total1)
+
+let total2 = (2+3) *4 
+console.log(total2)
+
+let average = (9.5 + 7 + 5) / 3
+console.log(average)
