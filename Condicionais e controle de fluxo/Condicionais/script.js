0b835e2cb1e6bf15fcadf871d48c92e10473a299
haru <ryanaraujoxd@gmail.com>
2024-10-30 09:25:56 -0300

    iniciando modulo de condicionais e controle de fluxo

---
+++ Condicionais e controle de fluxo/Condicionais/script.js
@@ -0,0 +1,11 @@
+// Operador Condicional ternário
+let age = 18
+console.log(age >=18 // Condição
+    ? "Você pode dirigir." // Se verdadeiro (True)
+    :"Você não pode dirigir.") // Se falso (False)
+
+    console.log(
+        age <18
+        ? "Você não pode dirigir."
+        : "Você pode dirigir." 
+    )
