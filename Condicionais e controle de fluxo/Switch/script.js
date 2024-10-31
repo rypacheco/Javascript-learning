2b0744fe3b41c84ad41e62047fea5b87913ed612
haru <ryanaraujoxd@gmail.com>
2024-10-31 10:45:09 -0300

    adicionando aula de Switch case

---
+++ Condicionais e controle de fluxo/Switch/script.js
@@ -0,0 +1,15 @@
+let option = 3
+
+switch(option){
+case 1:
+    console.log("Consultar pedido")
+    break
+case 2: 
+    console.log("Falar com atendente")
+    break
+case 3:
+    console.log("Cancelar Pedido")
+    break
+    default: 
+    console.log("Opção inválida.")
+}
